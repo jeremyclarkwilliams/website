569e7192f3f4c5a9eefce87f93231e6791099a33
Jeremy Williams <jeremy@jeremyclarkwilliams.com>
2018-08-24 13:59:44 -0700

    Add target _blank to links

--- partials/links.php
+++ partials/links.php
@@ -3,8 +3,8 @@
         <ul class="links">
             <li><a href="mailto:jeremy@jeremyclarkwilliams.com">Email</a></li>
             <li><a href="//www.linkedin.com/in/jwllms" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-            <li><a href="//github.com/jeremyclarkwilliams">GitHub</a></li>
-            <li><a href="files/jeremy-williams-resume-public.pdf">Resume</a></li>
+            <li><a href="//github.com/jeremyclarkwilliams" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+            <li><a href="files/jeremy-williams-resume-public.pdf" target="_blank" rel="noopener noreferrer">Resume</a></li>
             <?php if (!$links_top) : ?>
             <li class="hide-xs"><a href="#" id="back-to-top">Back to Top</a></li>
             <?php endif; ?>
