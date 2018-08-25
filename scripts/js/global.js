81e583b2a7f6f06ad007d48975258ac2efb65c90
Jeremy Williams <jeremy@jeremyclarkwilliams.com>
2018-08-24 17:47:00 -0700

    Fix scrolling issues

--- scripts/js/global.js
+++ scripts/js/global.js
@@ -1,22 +1,20 @@
 (() => {
 
     // declare variables
-    const $body = document.body;
     const $btnTop = document.getElementById('back-to-top');
-    let bodyScroll = $body.scrollTop;
 
     // basic scroll animation
     let scrollInterval,
         scrollDiff = scrollVal = 0;
 
     const scroller = (toPos) => {
-        bodyScroll = $body.scrollTop;
+        const bodyScroll = document.documentElement.scrollTop || document.body.scrollTop;
         // get difference between current position and destination
         scrollDiff = Math.abs(bodyScroll - toPos);
         // slow scroll at less than 200px to destination
         scrollVal = (scrollDiff < 200) ? -Math.ceil(scrollDiff / 2) : -100;
-        $body.scrollBy(0, scrollVal);
-        if (scrollDiff === 0) {
+        window.scrollBy(0, scrollVal);
+        if (scrollDiff <= 0) {
             // 0 difference, meaning we're at destination, so stop interval
             clearInterval(scrollInterval);
         }
