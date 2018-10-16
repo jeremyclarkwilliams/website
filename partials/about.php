0324245c5b4c6d1e1b3d18dc35fdc52ce7742dce
Jeremy Williams <jeremy@jeremyclarkwilliams.com>
2018-10-15 22:43:55 -0700

    Update about copy

--- partials/about.php
+++ partials/about.php
@@ -2,8 +2,9 @@
     <div class="container">
         <h3 class="heading">About</h3>
         <p>An innovative, customer-focused, analytical front-end developer, with a background in art &amp; design, offering over eight years of experience. Passion for clean, consistent, modular, and semantic UI development, with a drive to learn and quickly adapt to new technologies and frameworks. Known for excellent problem-solving skills and design-, detail-, and process-oriented behavior.</p>
-        <p>Currently seeking full-time, mid-to-senior level, front-end development/engineering opportunities.</p>
-        <p>Also available for cycling, movie discussion, board games, live music, and escape rooms.</p>
+        <!-- <p>Currently seeking full-time, mid-to-senior level, front-end development/engineering opportunities.</p> -->
+        <!-- <p>Also available for cycling, movie discussion, board games, live music, and escape rooms.</p> -->
+        <p>Available for cycling, movie discussion, board games, live music, and escape rooms.</p>
         <p>Based in Los Angeles, CA.</p>
     </div>
 </section>
